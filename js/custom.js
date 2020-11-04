7d15f98d52137e3ad94cdd3d31871a8f7880d5d6
Abuds Salam <51282606+smartwebcoder@users.noreply.github.com>
2020-11-04 15:23:11 +0600

    Create custom.js

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $(".search_toggle").on("click",function(){
+    $(this).toggleClass("close_search");
+    $(".search_form").toggleClass("zoom_search_form");
+  })
+  })
